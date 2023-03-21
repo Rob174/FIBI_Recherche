04634b4da6e5e0e104ba7910b347c760a839a2d8
Rob174 <robin.128.456@gmail.com>
2023-03-20 22:53:51 -0400

    Preparing doc for precise test explored flips for one neighbourhood

--- cpp_generation/FIBI/test/algorithms/localSearch/tsp.hpp
+++ cpp_generation/FIBI/test/algorithms/localSearch/tsp.hpp
@@ -30,7 +30,6 @@ public:
 	void on_glob_iter_end(const TSPSolutionContainer<>& c, const optional<double>& delta, const optional<TSPSwap>& s) override {
 	}
 	void on_iter_end(const TSPSolutionContainer<>& c, const optional<TSPSwap>& s) override {
-		// throw exception stop the algorithm
 		throw out_of_range("Stop the algorithm");
 	};
 	void on_end(const TSPSolutionContainer<>& c, const optional<TSPSwap>& s) override {
@@ -39,7 +38,7 @@ public:
 		return moves;
 	}
 };
-/** Test that the correct number of moves are explored in the 1-neighbourhood
+/** @brief{Test that the correct number of moves are explored in the 1-neighbourhood}
 * * Assuming that we have n towns, there are n-1 edges
 * * As swaps are build by pairs of edges the number of unique swaps is given by (n-1)C2. Contiguous edges are excluded.
 * * See documentation of TSPNeighbourhoodExplorer::explore_flips to see the explantion of the formula
@@ -52,7 +51,7 @@ void test_LocalSearch_flips_explored() {
 	random_device rd;
 	mt19937 gen(rd());
 	uniform_int_distribution<> dis_ints(1, max_int);
-	const int num_towns = dis_ints(gen);
+	const int num_towns = 5;
 
 	// Limit the tests to 2D for now (only usage in the codebase)
 	const int num_dims = 2;
@@ -94,3 +93,47 @@ void test_LocalSearch_flips_explored() {
 		}
 	}
 }
+
+/** @brief{Test that the correct moves are explored with following instances}
+							 FI STOP                                  BI CHOICE
+
+   Cost=43.6            Cost=39.4               Cost=40.2             Cost=35.4         Cost=39.5            Cost=43.6
+			   [2]                     [2]                 [2]                  [2]                  [2]                 [2]
+			 *O                      *O                  *O         @@@@@@@@@@@O                 @@@O        @@@@@@@@@@@O
+[4]         ** *        [4]         **@     [4]         ** *     [4]@           *     [4]        @   *    [4]@        **
+***O        *  *        ***O        * @        O        *  *        O           *     ***O       @   *    ***O        *
+*   **      *  *        *   **      * @        @**      *  *         **         *     *  @       @   *    *           *
+*    ***[0] *  *        *    ***[0] * @        @ ***[0] *  *          ***[0]    *     *  @    [0]@   *    *       [0] *
+*       O  * **         *       O@@*@@@        @    O  * **              O    **      *  @    O@@@ **     *       O@@@*
+*      **  * *          *          *           @    @  * *              **    *       *  @   **    *      *      **  @
+*     **  *  *          *        **            @    @ *  *             **     *       *  @  **     *      *     **  *@
+*    **  *  **          *      **              @    @*  **            **     **       *  @ **     **      *    **  * @
+*    *  *  **           *    **                @    @  **             *     **        *  @ *     **       *    *  *  @
+*   O***  **            *   O@@@@@@            @O***@ **             O     **         *  @@     **        *   O***   @
+*[1]      O             *[1]      O          [1]    @@O           [1] @@@@@O          *[1]      O         *[1]      O@
+***********[3]          ***********[3]                 [3]                  [3]       ***********[3]      ***********[3]
+							 0,2                  0,3                  1,3                 1,4                2,4
+
+
+						(0,0) (-4,-5)   (2,-6)    (6,5)   (-5,3)
+				 ┌─────┬───────────────────────────────────────────┐
+				 │     │  0       1        2        3        4     │
+				 ├─────┼──────┬───────┬────────┬─────────┬─────────┤
+			(0,0)│  0  │  0   │  6.4  │   7.8  │   6.3   │  5.8    │
+				 │     ├──────┼───────┼────────┼─────────┼─────────┤
+				 │     │      │       │        │         │         │
+		  (-4,-5)│  1  │ 6.4  │   0   │  14.1  │   6.1   │  8.1    │
+				 │     ├──────┼───────┼────────┼─────────┼─────────┤
+				 │     │      │       │        │         │         │
+			(6,5)│  2  │ 7.8  │ 14.1  │    0   │   11.7  │  11.2   │
+				 │     ├──────┼───────┼────────┼─────────┼─────────┤
+				 │     │      │       │        │         │         │
+		   (2,-6)│  3  │ 6.3  │  6.1  │  11.7  │    0    │  11.4   │
+				 │     ├──────┼───────┼────────┼─────────┼─────────┤
+				 │     │      │       │        │         │         │
+		   (-5,3)│  4  │ 5.8  │  8.1  │  11.2  │   11.4  │   0     │
+				 │     │      │       │        │         │         │
+				 └─────┴──────┴───────┴────────┴─────────┴─────────┘
+
+# done with https://asciiflow.com
+* */
